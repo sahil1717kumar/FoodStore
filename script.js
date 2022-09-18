79eb058cbee144067179ca8eb8df86b2304057d4
shail kumar <sahil1717kumar@gmail.com>
2022-09-18 20:24:55 +0530

    video section complete

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ const mobile_nav = document.querySelector(".mobile-navbar-btn");
 const nav_header = document.querySelector(".header");
 const body = document.querySelector("body");
 
+const video_button = document.querySelector(".video-button")
+const content = document.querySelector(".content");
+var flag = 1;
+video_button.addEventListener("click",function(){
+  if(flag){
+    content.play()
+    flag = 0;
+  }
+  else{
+    content.pause()
+    flag = 1;
+  }
+  
+})
 const toggleNavbar = () => {
   
   nav_header.classList.toggle("active");
