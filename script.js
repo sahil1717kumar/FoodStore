57610bb4c7958e4ba0fd78167b7ae2aa9444d455
Sanskar Srivastava <sanskar0703@gmail.com>
2022-10-11 15:20:38 +0530

    complete web view

--- script.js
+++ script.js
@@ -5,6 +5,35 @@ const body = document.querySelector("body");
 const video_button = document.querySelector(".video-button")
 const content = document.querySelector(".content");
 var flag = 1;
+
+const bbody = document.querySelector("body")
+const modalBack = document.querySelector(".modal-back")
+const menuBtn = document.querySelector(".back-to-menu-btn")
+const searchContainer = document.querySelector(".order-modal")
+const cartContainer = document.querySelector(".cart-container")
+const cart = document.querySelector(".cart-btn")
+const search = document.querySelector(".search-btn")
+const cancelBtn = document.querySelector(".cancel-order")
+menuBtn.addEventListener("click",function(){
+  bbody.style.overflow = "auto"
+  cartContainer.style.display = "none"
+  modalBack.style.display = "none"
+})
+cart.addEventListener("click",function(){
+  bbody.style.overflow = "hidden"
+  modalBack.style.display = "block"
+  cartContainer.style.display = "flex"
+})
+cancelBtn.addEventListener("click",function(){
+  bbody.style.overflow = "auto"
+  searchContainer.style.display = "none"
+  modalBack.style.display = "none"
+})
+search.addEventListener("click",function(){
+  bbody.style.overflow = "hidden"
+  modalBack.style.display = "block"
+  searchContainer.style.display = "block"
+})
 video_button.addEventListener("click",function(){
   if(flag){
     content.play()
